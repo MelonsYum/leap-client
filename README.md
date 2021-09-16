1d368b80715973ef6ba3c878492d84d2108b5fff
danny-125 <90357372+danny-125@users.noreply.github.com>
2021-09-16 17:16:01 -0600

    Added leap full src

--- README.md
+++
@@ -1,2 +0,0 @@
-# leap-client
-So, the leap client source code fell into the wrong hands, brandonthebest is a shitty developer, please note some of this may be broken as this is a test build of leap.
--- leap/util/ConfigUtil.java
+++
@@ -1,9 +0,0 @@
-package leap.util;
-
-public class ConfigUtil {}
-
-
-/* Location:              C:\Users\wyatt\Downloads\Leap-Client.jar!\lea\\util\ConfigUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
